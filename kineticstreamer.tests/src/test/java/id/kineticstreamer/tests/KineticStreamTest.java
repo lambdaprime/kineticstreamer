ad8c08596c10319564c05b8a47278cf494ad6fa7
lambdaprime <id.blackmesa@gmail.com>
2020-05-25 14:37:16 -0700

    Combining all read/write tests in one class since they all differ with params only

--- kineticstreamer.tests/src/test/java/id/kineticstreamer/tests/KineticStreamTest.java
+++ kineticstreamer.tests/src/test/java/id/kineticstreamer/tests/KineticStreamTest.java
@@ -7,6 +7,7 @@ package id.kineticstreamer.tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -14,12 +15,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import id.kineticstreamer.DefaultKineticDataInput;
+import id.kineticstreamer.DefaultKineticDataOutput;
 import id.kineticstreamer.InputStreamByteList;
 import id.kineticstreamer.KineticStreamReader;
+import id.kineticstreamer.KineticStreamWriter;
+import id.kineticstreamer.OutputStreamByteList;
 import id.kineticstreamer.tests.streamed.Book;
 import id.kineticstreamer.tests.streamed.StringMessage;
 
-public class KineticStreamReaderTest {
+public class KineticStreamTest {
 
     static Stream<List> dataProvider() {
         return Stream.of(
@@ -31,7 +35,18 @@ public class KineticStreamReaderTest {
 
     @ParameterizedTest
     @MethodSource("dataProvider")
-    public void test(List testData) throws Exception {
+    public void testWrite(List testData) {
+        var b = testData.get(1);
+        OutputStreamByteList collector = new OutputStreamByteList();
+        var dos = new DefaultKineticDataOutput(new DataOutputStream(collector));
+        var ks = new KineticStreamWriter(dos);
+        ks.write(b);
+        assertEquals(testData.get(0), collector.asHexString());
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void testRead(List testData) throws Exception {
         var collector = new InputStreamByteList((String)testData.get(0));
         var dis = new DefaultKineticDataInput(new DataInputStream(collector));
         var ks = new KineticStreamReader(dis);
